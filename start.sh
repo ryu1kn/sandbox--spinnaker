973ed5046b350fa4f8e5b0476d194872bc03cfd5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-24 09:32:28 +1000

    Wait until Tiller gets deployed

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 minikube start
-helm init
+helm init --wait
 helm install --name my-spinnaker stable/spinnaker --timeout 600
 kubectl expose deployment spin-deck --type=NodePort --name=spin-deck-2
 kubectl expose deployment spin-gate --type=NodePort --name=spin-gate-2  # For spin cli
