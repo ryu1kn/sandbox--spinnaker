af797caee52f986a3328ab303378946002397fa5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-08-05 19:14:16 +1000

    Billing should be enabled first

--- tutorial/0-setup-project.sh
+++ tutorial/0-setup-project.sh
@@ -7,5 +7,5 @@ set -euo pipefail
 gcloud projects create $PROJECT
 gcloud config set project $PROJECT
 
-gcloud services enable container.googleapis.com cloudbuild.googleapis.com sourcerepo.googleapis.com
 gcloud alpha billing projects link $PROJECT --billing-account $GCP_BILLING_ACCOUNT_ID
+gcloud services enable container.googleapis.com cloudbuild.googleapis.com sourcerepo.googleapis.com
