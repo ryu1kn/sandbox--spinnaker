46019791778804f2e6c7d172001cf578f08876e4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-08-08 23:34:12 +1000

    I keep using the existing project...

--- tutorial/0-setup-project.sh
+++ tutorial/0-setup-project.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 . "$(dirname $0)/config.sh"
 
-gcloud projects create $PROJECT
+# gcloud projects create $PROJECT
 gcloud config set project $PROJECT
 
 gcloud alpha billing projects link $PROJECT --billing-account $GCP_BILLING_ACCOUNT_ID
